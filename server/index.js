38119d87f77e8e23c2af98d1bc1118234caef581
JonnyWise25 <jwise25@cmc.edu>
2024-09-17 21:46:52 -0700

    Created basic folder structure

---
+++ server/index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+  res.send('Hello from server');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
